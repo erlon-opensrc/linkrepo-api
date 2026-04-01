ba0597f24ac7d3041c1467047946ff07e6871dbf
erlon <>
2026-04-01 15:22:13 -0300

    hash password during  user creation

--- app/database/crud_user.py
+++ app/database/crud_user.py
@@ -1,12 +1,17 @@
 from fastapi import HTTPException, status
 from sqlmodel import Session
 from sqlalchemy.exc import IntegrityError
+from argon2 import PasswordHasher
 
 from ..models.user import UserDB, UserCreate
 
+
+pwd_hasher = PasswordHasher()
+
+
 def create(session: Session, user: UserCreate) -> UserDB:
     new_user = UserDB.model_validate(user)
-    # TODO: hash password
+    new_user.password = pwd_hasher.hash(new_user.password)
     try:
         session.add(new_user)
         session.commit()
